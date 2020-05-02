47a2e9194efac5c5ac80c20c988cb3d97a9d1be6
Walid7627 <wkhali95360@gmail.com>
2020-05-02 13:45:54 +0200

    Add files via upload

--- sources/backend/src/main/java/com/sigma/DemoApplication.java
+++ sources/backend/src/main/java/com/sigma/DemoApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
+//@SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class DemoApplication {
 
   public static void main(String[] args) {
